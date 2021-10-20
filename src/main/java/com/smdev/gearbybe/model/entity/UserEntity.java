e53d89d8cd138a725b171616176bc25bf7d6d523
ALazyGuy <razorprogrammer@gmail.com>
2021-10-20 12:14:04 +0300

    feat: Added user entity

---
+++ src/main/java/com/smdev/gearbybe/model/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package com.smdev.gearbybe.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String fullName;
+    private String password;
+    private String phoneNumber;
+    private double cash;
+    private String address;
+
+}
