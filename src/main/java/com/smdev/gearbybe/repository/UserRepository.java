779de59ff7885e4b91bde2885c0343dddfcb375a
ALazyGuy <razorprogrammer@gmail.com>
2021-10-20 12:16:37 +0300

    feat: Implemented user repository

---
+++ src/main/java/com/smdev/gearbybe/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.smdev.gearbybe.repository;
+
+import com.smdev.gearbybe.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
