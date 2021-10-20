388305e5f76ef05800091be5cc91ea5c7e10114e
ALazyGuy <razorprogrammer@gmail.com>
2021-10-20 12:11:03 +0300

    init: Created project

---
+++ src/main/java/com/smdev/gearbybe/GearByBeApplication.java
@@ -0,0 +1,13 @@
+package com.smdev.gearbybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearByBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearByBeApplication.class, args);
+	}
+
+}
