4a19776ab61f480b7b955d5dc77ed0e474eaa5a3
ALazyGuy <razorprogrammer@gmail.com>
2021-10-27 03:19:24 +0300

    feat: Implemented documentation for part controller

--- src/main/java/com/smdev/gearbybe/controller/PartController.java
+++ src/main/java/com/smdev/gearbybe/controller/PartController.java
@@ -3,8 +3,14 @@ package com.smdev.gearbybe.controller;
 import com.smdev.gearbybe.model.dto.PartCreateRequest;
 import com.smdev.gearbybe.model.dto.PartReserveRequest;
 import com.smdev.gearbybe.model.dto.PartSearchRequest;
+import com.smdev.gearbybe.model.dto.response.OrderResponse;
 import com.smdev.gearbybe.model.entity.PartEntity;
 import com.smdev.gearbybe.service.PartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,6 +33,12 @@ public class PartController {
         this.partService = partService;
     }
 
+    @Operation(summary = "Get all parts")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Returned successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = PartEntity.class))}),
+            @ApiResponse(responseCode = "204", description = "No parts found")
+    })
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<PartEntity>> getAll(){
         List<PartEntity> parts = partService.getAll();
@@ -36,6 +48,12 @@ public class PartController {
         return ResponseEntity.ok(parts);
     }
 
+    @Operation(summary = "Search parts by optional parameters")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Returned successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = PartEntity.class))}),
+            @ApiResponse(responseCode = "204", description = "No parts found")
+    })
     @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<PartEntity>> search(@Valid @RequestBody PartSearchRequest partSearchRequest){
         List<PartEntity> parts = partService.search(partSearchRequest);
@@ -45,6 +63,12 @@ public class PartController {
         return ResponseEntity.ok(parts);
     }
 
+    @Operation(summary = "Create a new part")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Created successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = PartEntity.class))}),
+            @ApiResponse(responseCode = "409", description = "Part with same name is already exists")
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PartEntity> create(@Valid @RequestBody PartCreateRequest partCreateRequest){
         Optional<PartEntity> partEntity = partService.create(partCreateRequest);
@@ -54,6 +78,12 @@ public class PartController {
         return ResponseEntity.status(HttpStatus.CREATED).body(partEntity.get());
     }
 
+    @Operation(summary = "Update part amount")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Updated successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = PartEntity.class))}),
+            @ApiResponse(responseCode = "404", description = "Part not found")
+    })
     @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PartEntity> updateAmount(@Valid @RequestBody PartReserveRequest partReserveRequest){
         Optional<PartEntity> partEntity = partService.updateAmountOfParts(partReserveRequest);
